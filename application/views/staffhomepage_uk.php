65b76357885ddc4ae3a869ee3a302e54fd106a73
rkittu08 <sakhamuriramakrishna@hotmail.com>
2015-11-05 09:55:04 -0600

    IMS project
    on project implementation  day 1

---
+++ application/views/staffhomepage_uk.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE>
+<html>
+    
+    <body>
+   <?php $this->load->view('includes/staff_navbar1'); ?>
+
+    
+   
+         <?php $this->load->view('viewworkorder_uk'); ?>
+                     
+    
+             
+            
+ 
+<?php $this->load->view('includes/footer'); ?>
+</body>
+</html>
